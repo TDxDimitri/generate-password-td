39e8fe6456e7dbe092161215549f190cd43546fe
TDxDimitri <talleu.dimitri@outlook.fr>
2023-02-09 11:54:45 +0100

    prog ok

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function generer_password(){
 
 
 
-    let ok = 'azertyupqsdfghjkmwxcvbn23456789AZERTYUPQSDFGHJKMWXCVBN[:punct]';
+    let ok = 'azertyupqsdfghjkmwxcvbn23456789AZERTYUPQSDFGHJKMWXCVBN@$#*![:punct]';
     let mdp='';
     var input = document.getElementById("in").value;
         for(i=0;i<input;i++){
